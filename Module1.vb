fa7c19d6d754ab551465c73b516671ca0eb022fa
Themen <thesolve@mail.ru>
2023-03-14 16:12:20 +0300

    Добавил ожидание появления файла. Добавил время до выключения после обнаружения файла или завершения процессов.

--- Module1.vb
+++ Module1.vb
@@ -5,13 +5,28 @@ Module Module1
 	Private SETTINGS_FILE_NAME As String = "settings.txt"
 	Sub Main()
 		Dim sr As New IO.StreamReader(IO.Path.Combine(My.Application.Info.DirectoryPath, SETTINGS_FILE_NAME))
-		Dim procNames() As String = sr.ReadLine().Split(" "c)
+
+		Dim procsOrFiles() As String = sr.ReadLine().Split(";"c)
+		Dim isFiles As Boolean = False
+		For Each entry As String In procsOrFiles
+			If entry.Contains(":") Then
+				isFiles = True
+				Exit For
+			End If
+		Next
 		Dim period As Integer = Integer.Parse(sr.ReadLine())
+		Dim timeAfterTrigger As Integer = Integer.Parse(sr.ReadLine())
+		Dim testFunction As TestFunction
+		If isFiles Then
+			testFunction = AddressOf hasNoFile
+		Else
+			testFunction = AddressOf hasProc
+		End If
 		Do
-			If noOneProcs(procNames) Then
+			If test(procsOrFiles, testFunction) Then
 				Console.Write("+")
-				Threading.Thread.Sleep(3000) 'Ждём, вдруг появится
-				If noOneProcs(procNames) Then Exit Do
+				Threading.Thread.Sleep(timeAfterTrigger) 'Ждём, вдруг появится
+				If test(procsOrFiles, testFunction) Then Exit Do
 			End If
 			Console.Write("-")
 			Threading.Thread.Sleep(period)
@@ -24,13 +39,17 @@ Module Module1
 		Shell("shutdown /s /f /t 0")
 	End Sub
 
+	Private Function hasNoFile(file As String) As Boolean
+		Return Not IO.File.Exists(file)
+	End Function
 	Private Function hasProc(procName As String) As Boolean
 		Return Process.GetProcessesByName(procName).Length > 0
 	End Function
-	Private Function noOneProcs(procNames() As String) As Boolean
+	Delegate Function TestFunction(name As String) As Boolean
+	Private Function test(procsOrFiles() As String, tf As TestFunction) As Boolean
 		Dim flag As Boolean = True
-		For Each proc As String In procNames
-			flag = flag And Not hasProc(proc)
+		For Each entry As String In procsOrFiles
+			flag = flag And Not tf(entry)
 			If Not flag Then Exit For
 		Next
 		Return flag
